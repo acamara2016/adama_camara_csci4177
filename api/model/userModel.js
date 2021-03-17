d1a4cdd912857527482261661c040872bcc11003
Adama Camara <48602776+acamara2016@users.noreply.github.com>
2021-03-17 18:00:39 -0300

    tutorial 7 commit

--- api/model/userModel.js
+++ api/model/userModel.js
@@ -1,92 +1,13 @@
-const users = [
-    {
-        "_id": "5ef19102f384e92de40f8c88",
-        "userName": "Vali Shaik",
-        "email": "vali@dal.ca",
-        "__v": 0
-    },
-    {
-        "_id": "5ef28cecb44a3b00170d1f2a",
-        "userName": "Vamsi",
-        "email": "vamshi@dal.ca",
-        "__v": 0
-    },
-    {
-        "_id": "5efba7c6d37d0816ed03b63f",
-        "userName": "Satya Kumar",
-        "email": "satya@gmail.com"
-    },
-    {
-        "_id": "5efba7fad37d0816ed03b640",
-        "userName": "sai",
-        "email": "sai@gmail.com"
-    },
-    {
-        "_id": "5efba82fd37d0816ed03b641",
-        "userName": "pavan",
-        "email": "pavan@gmail.com"
-    },
-    {
-        "_id": "5efba843d37d0816ed03b642",
-        "userName": "sunil",
-        "email": "sunil@gmail.com"
-    },
-    {
-        "_id": "5efba85cd37d0816ed03b643",
-        "userName": "sneh",
-        "email": "sneh@gmail.com"
-    },
-    {
-        "_id": "5efba86ed37d0816ed03b644",
-        "userName": "aakash",
-        "email": "aakash@gmail.com"
-    },
-    {
-        "_id": "5efba887d37d0816ed03b645",
-        "userName": "Tob",
-        "email": "tob@gmail.com"
-    },
-    {
-        "_id": "5efba89ad37d0816ed03b646",
-        "userName": "Rob",
-        "email": "Rob@gmail.com"
-    },
-    {
-        "_id": "5efbb8b3006580301cbd39cf",
-        "userName": "John",
-        "email": "john.wick@gmail.com",
-        "__v": 0
-    },
-    {
-        "_id": "5efbb8c1006580301cbd39d0",
-        "userName": "Jack",
-        "email": "jack.ryan@gmail.com",
-        "__v": 0
-    },
-    {
-        "_id": "5efbb8cf006580301cbd39d1",
-        "userName": "Ethan",
-        "email": "ethan.hunt@gmail.com",
-        "__v": 0
-    },
-    {
-        "_id": "5efbb8dd006580301cbd39d2",
-        "userName": "jack",
-        "email": "jack.reacher@gmail.com",
-        "__v": 0
-    },
-    {
-        "_id": "6041025a088fff0017f9fd0d",
-        "userName": "aadesh",
-        "email": "aadesh@dal.ca",
-        "__v": 0
-    },
-    {
-        "_id": "60412e0a2d22aa0017b52621",
-        "userName": "aadesh123",
-        "email": "aadesh@dal.com",
-        "__v": 0
-    }
-]
+const mongoose = require('mongoose');
 
-module.exports = users;
+const userSchema = mongoose.Schema({
+    
+    _id: mongoose.Schema.Types.ObjectId,
+    email:{
+        type: String,
+        required: true
+    },
+    userName: String
+})
+
+module.exports = mongoose.model("Users", userSchema)
