5f8eec3b6b6dc25953b176ad412c24b734c35ff6
Adama Camara <48602776+acamara2016@users.noreply.github.com>
2021-03-17 18:31:24 -0300

    final submission for tutorial 7

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const http = require('http');
 const app = express();
 const userRoute = require('./api/routes/useRoute');
-
+const port = process.env.PORT || 3000;
 const uri = "mongodb+srv://dbUser:I1f7zKkhhhjZjRfh@cluster0.8wh3l.mongodb.net/tutorial7?retryWrites=true&w=majority";
+
 mongoose.connect(uri,{
     useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.use('/user', userRoute);
+const server = http.createServer(app);
 
+server.listen(port);
 
 module.exports = app;
